d545ca3330e48f9811c776cdda7f3b268d31d54f
Vexwe <deyvid.mota.cruz@gmail.com>
2022-12-22 11:40:09 -0300

    Inicio do Projeto

---
+++ Olamundoswing/src/main/java/com/mycompany/olamundoswing/Olamundoswing.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.olamundoswing;
+
+/**
+ *
+ * @author Hisol
+ */
+public class Olamundoswing {
+
+    public static void main(String[] args) {
+        System.out.println("Salve salve quebrada");
+    }
+    
+
+}
---
+++ Primeiroprograma/src/main/java/com/mycompany/primeiroprograma/Primeiroprograma.java
@@ -0,0 +1,8 @@
+package com.mycompany.primeiroprograma;
+
+public class Primeiroprograma {
+
+    public static void main(String[] args) {
+        System.out.print("Salve salve quebras");
+    }
+}
